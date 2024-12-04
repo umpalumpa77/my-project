44e462e85c425d6ab5c4c02bbb8eccbdd59b88b2
dimass <dmitri.burav77@gmail.com>
2024-12-04 20:25:24 +0300

    main api

--- api/main_api.py
+++ api/main_api.py
@@ -1,16 +1,27 @@
-import requests
-import json
+# import requests
+# import json
+# import pprint
 
-response = requests.get("https://randomuser.me/api")
-if response. ok:
+# response = requests.get("https://randomuser.me/api")
+# if response.ok:
+#     data = response.json()
+#     user = data["results"][0]
+#     print(f"имя: {user['name']['first']}")
+#     print(f"Страна: {user['location']['country']}")
+#     print("-"* 30)
+#     print("printing data")
+#     pprint.pprint(data)
+#     print("Printing user")
+#     pprint.pprint(user)
+# else:
+#     print("Ошибка получения данных")
+
+import requests 
+
+response = requests.get("htpps://apo.quotable.io/random")
+
+if response.status_code == 200:
     data = response.json()
-    user = data["results"][0]
-    print(f"имя: {user['name']['first']}")
-    print(f"Страна: {user['location']['country']}")
-    print("-"* 30)
-    print("printing data")
-    pprint.pprint(data)
-    print("Printing user")
-    pprint.pprint(user)
+    print(f"Случайная цитата: '{data['content']}' - {data['author']}")
 else:
-    print("Ошибка получения данных")
+    print("Не удалось получить цитату")
